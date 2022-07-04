b44e8098d93f22a98562c075f607973071323baa
Billy Jacoby <billyjacoby@gmail.com>
2022-07-04 15:20:21 -0400

    Copied example to SendRandom

--- src/SendRandom.tsx
+++ src/SendRandom.tsx
@@ -1,34 +1,54 @@
-import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
-import React, { useCallback } from 'react';
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+  TransactionSignature,
+} from '@solana/web3.js';
+import { FC, useCallback } from 'react';
 
-export const SendRandom = () => {
+export const SendRandom: FC = () => {
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
 
   const onClick = useCallback(async () => {
-    if (!publicKey) throw new WalletNotConnectedError();
+    if (!publicKey) {
+      console.error('error', 'Wallet not connected!');
+      return;
+    }
 
-    const transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: publicKey,
-        toPubkey: Keypair.generate().publicKey,
-        lamports: 1,
-      })
-    );
-    transaction.recentBlockhash = (
-      await connection.getLatestBlockhash()
-    ).blockhash;
+    let signature: TransactionSignature = '';
+    try {
+      const transaction = new Transaction().add(
+        new TransactionInstruction({
+          data: Buffer.from(
+            'Hello, from the Solana Wallet Adapter example app!'
+          ),
+          keys: [],
+          programId: new PublicKey(
+            'MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr'
+          ),
+        })
+      );
 
-    const signature = await sendTransaction(transaction, connection);
+      signature = await sendTransaction(transaction, connection);
+      console.info('info', 'Transaction sent:', signature);
 
-    await connection.confirmTransaction(signature, 'processed');
-  }, [publicKey, sendTransaction, connection]);
+      await connection.confirmTransaction(signature, 'processed');
+      console.info('success', 'Transaction successful!', signature);
+    } catch (error: any) {
+      console.error(
+        'error',
+        `Transaction failed! ${error?.message}`,
+        signature
+      );
+      return;
+    }
+  }, [publicKey, connection, sendTransaction]);
 
   return (
     <button onClick={onClick} disabled={!publicKey}>
-      Send 1 lamport to a random address!
+      Send Transaction (devnet)
     </button>
   );
 };
